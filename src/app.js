591e130e70cd682a55a21ab6a3f8e657894aaa55
Asante-Nsiah <88829934+Asante-Nsiah@users.noreply.github.com>
2023-03-02 16:23:42 +0000

    Login page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const routes = require('./route/routing');
+const ejs = require('ejs');
+const { dirname } = require('path');
+const path = require('path')
+
 
 const app = express();
 app.use(express.json())
 
+app.use(routes)
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views') );
+app.use(express.static(path.join(__dirname, './../public')));
 
 
 
--- src/route/routing.js
+++ src/route/routing.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const auth = require('../controller/authCtrl');
 
 const router = express.Router();
+
+router.get('/login', auth.login)
+
+
+
+
+
+module.exports = router;
